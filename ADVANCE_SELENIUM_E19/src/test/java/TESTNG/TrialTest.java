b22b951135ed53ffd6dd2e63fa1df38e9d99e302
AnjuKanjirathingal <117965948+AnjuKanjirathingal@users.noreply.github.com>
2025-04-15 20:16:37 +0530

    Update TrialTest.java

--- ADVANCE_SELENIUM_E19/src/test/java/TESTNG/TrialTest.java
+++ ADVANCE_SELENIUM_E19/src/test/java/TESTNG/TrialTest.java
@@ -8,11 +8,11 @@ public class TrialTest extends TestNgBaseClass
 {
 	@Test(priority = 2, invocationCount = 3)
 	public void printMyStatement() {
-		/*
-		 * int i=1/0; it is arithamatical exception which we are not going to handle it
-		 * for now this will lead login() to get failed and as a result , the other two
-		 * methods get skipped since it is dependant on login()
-		 */
+		//Pulling back toEclipse 1
+		  int i=1/0; it is arithamatical exception which we are not going to handle it
+		  for now this will lead login() to get failed and as a result , the other two
+		  methods get skipped since it is dependant on login()
+		 
 		System.out.println("test my TESTNG");
 	}
 
