f451ffe98c5b5bc184fe89630fe0ddd4c1fd57e5
AnjuKanjirathingal <117965948+AnjuKanjirathingal@users.noreply.github.com>
2025-04-16 20:35:14 +0530

    BranchingPushByANju

---
+++ ADVANCE_SELENIUM_E19/src/test/java/GitHubPractiCing/BranchingTrialOnGithub.java
@@ -0,0 +1,14 @@
+package GitHubPractiCing;
+
+public class BranchingTrialOnGithub 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+		System.out.println("cant directly push to the Main branch which is because branching is used to avoid unnecessary conflicts.But we can Pull from the main branch directly");
+		System.out.println("NewBranch Created As Anju.We are Pushing it to the branche created instead of master branch.so please select branch name while pushing");
+	}
+
+}
